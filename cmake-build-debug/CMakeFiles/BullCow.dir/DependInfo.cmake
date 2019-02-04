0285d25bce11bc6466655f6a7f788f00ea7303db
devlwkyan <devlwkyan@protonmail.com>
2019-02-04 14:59:02 -0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/BullCow.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/devlwkyan/Documents/Programming/C++/Gaming/BullCow/BullCow/BullCow.cpp" "/home/devlwkyan/Documents/Programming/C++/Gaming/BullCow/cmake-build-debug/CMakeFiles/BullCow.dir/BullCow/BullCow.cpp.o"
+  "/home/devlwkyan/Documents/Programming/C++/Gaming/BullCow/BullCow/pch.cpp" "/home/devlwkyan/Documents/Programming/C++/Gaming/BullCow/cmake-build-debug/CMakeFiles/BullCow.dir/BullCow/pch.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../BullCow"
+  "../BullCow/Debug"
+  "../BullCow/x64"
+  "../BullCow/x64/Debug"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BullCow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BullCow.dir/BullCow/BullCow.cpp.o"
+  "CMakeFiles/BullCow.dir/BullCow/pch.cpp.o"
+  "BullCow.pdb"
+  "BullCow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BullCow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
